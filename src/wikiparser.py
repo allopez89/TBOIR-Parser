fa9422f1c04165ba804c3ed76d9f429830510223
axellopez89 <axellpz89@hotmail.com>
2021-06-11 19:06:50 -0400

    Added Challenges, Chapters, Minibosses, Monsters, Rooms, Pools, Transformation and Trinkets

--- src/wikiparser.py
+++ src/wikiparser.py
@@ -34,7 +34,6 @@ def GetBatteries_Chests_Sacks(url):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find("div", class_="mw-parser-output")
-    #tbody = results.find("tbody")
     trow = results.find_all("span", id=True)
 
     for i, tcell in enumerate(trow):
@@ -45,6 +44,17 @@ def GetBatteries_Chests_Sacks(url):
     for i in range(0, len(discard)):
         if discard[i] in trow: trow.remove(discard[i])
     return trow
+
+def GetPools(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find("table", class_="nav-header")
+    tbody = results.find("tbody")
+    tcell = tbody.find_all("a")
+
+    for i, cell in enumerate(tcell):
+        tcell[i] = cell["title"]
+    return tcell
 #endregion
 
 Actives = tboi.categorymembers("Activated collectibles", None, False)
@@ -61,6 +71,14 @@ Pills = GetHearts_Bombs_Pills_Cards_Runes("https://bindingofisaacrebirth.fandom.
 Cards_Runes = GetHearts_Bombs_Pills_Cards_Runes("https://bindingofisaacrebirth.fandom.com/wiki/Cards_and_Runes")
 Chests = GetBatteries_Chests_Sacks("https://bindingofisaacrebirth.fandom.com/wiki/Chests")
 Sacks = GetBatteries_Chests_Sacks("https://bindingofisaacrebirth.fandom.com/wiki/Sacks")
+Challenges = tboi.categorymembers("Challenges", None, False)
+Chapters = tboi.categorymembers("Chapters", None, False)
+Mini_Bosses = tboi.categorymembers("Mini-bosses", None, False)
+Monsters = tboi.categorymembers("Monsters", None, False)
+Rooms = tboi.categorymembers("Rooms", None, False)
+Pools = GetPools("https://bindingofisaacrebirth.fandom.com/wiki/Item_Pool")
+Transformations = tboi.categorymembers("Transformations", None, False)
+Trinkets = tboi.categorymembers("Trinkets", None, False)
 
 #region Parse Characters, Achievements, Bosses
 # Delete not needed characters
@@ -75,12 +93,43 @@ for i, s in enumerate(Achievements):
     Achievements[i] = s.replace("File:", "").replace(".png", "").replace(" icon", "")
 
 # Delete not needed and parse bosses
-Bosses.append("Angel")
-Bosses.sort()
 Bosses[:] = [x for x in Bosses if not "Bosses" in x]
 if "Seven Deadly Sins" in Bosses: Bosses.remove("Seven Deadly Sins")
 for i, s in enumerate(Bosses):
     Bosses[i] = s.replace(" (Boss)", "")
+
+# Parse Challenges
+for i, s in enumerate(Challenges):
+    Challenges[i] = s.replace(" (Challenge)", "")
+
+# Delete not needed and parse Chapters (Floors)
+discardCategory = ["Greed Mode", "The Void/Locating Delirium"]
+for i, s in enumerate(Chapters):
+    Chapters[i] = s.replace(" (Floor)", "")
+for x in range(0, len(discardCategory)):
+    if discardCategory[x] in Chapters: Chapters.remove(discardCategory[x])
+
+# Delete not needed and parse Mini Bosses
+discardMiniBoss = ["All Mini-Bosses (Bosses)", "Tainted Jacob"]
+Mini_Bosses.append("Dark Esau")
+Mini_Bosses.sort()
+for x in range(0, len(discardMiniBoss)):
+    if discardMiniBoss[x] in Mini_Bosses: Mini_Bosses.remove(discardMiniBoss[x])
+
+# Parse Monsters
+if "Monsters" in Monsters: Monsters.remove("Monsters")
+for i, s in enumerate(Monsters):
+    Monsters[i] = s.replace(" (Enemy)", "")
+
+# Parse Item Pools
+Pools[:] = [x for x in Pools if ("Item Pool" in x) and not ("Greed Mode Item Pool" in x)]
+for i, s in enumerate(Pools):
+    Pools[i] = s.replace(" (Item Pool)", "")
+
+# Parse Transformations
+if "Transformations" in Transformations: Transformations.remove("Transformations")
+for i, s in enumerate(Transformations):
+    Transformations[i] = s.replace(" (Transformation)", "")
 #endregion
 
-print(Sacks)
+print(Trinkets)
