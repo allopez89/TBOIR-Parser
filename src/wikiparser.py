a017fc09af420ddb7d42bf4ead4c40d0268cc778
Jose <joseamendozajimenez@hotmail.com>
2021-06-12 13:51:11 -0500

    Testing images

--- src/wikiparser.py
+++ src/wikiparser.py
@@ -1,8 +1,9 @@
 from mediawiki import MediaWiki
 from bs4 import BeautifulSoup
 import requests
+import Titles
 
 tboi = MediaWiki(url="https://bindingofisaacrebirth.fandom.com/api.php", user_agent="TBOIR-WikiParser/0.1.0 (axellpz89@hotmail.com) pymediawiki/0.7.0")
 sections = tboi.page("A Pony").sections
 sections.remove("")
-print(sections)
+print(Titles.GetCharacters())
